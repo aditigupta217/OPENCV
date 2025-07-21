ad9a40240457d023d1080bff369c7d0d77d66350
aditigupta217 <aditigupta221177@gmail.com>
2025-07-21 23:12:59 +0530

    uff

--- filtering/threshold.py
+++ filtering/threshold.py
@@ -5,6 +5,6 @@ image = cv2.imread("/Users/aditi/Desktop/opencv/phase2/ChatGPT Image Mar 29, 202
 ret, threshold = cv2.threshold(image,120,50,cv2.THRESH_BINARY)
  
 cv2.imshow("image",image)
-cv2.imshow("threshold image", threshold)
+cv2.imshow("threshold image is ", threshold)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
